8d68e72cb95338ee32556aa29516cfb8ff86121e
YoutaSeki <uyu9480s@outlook.jp>
2019-06-25 08:40:16 +0000

    fixed global_to_local

--- script/coordinate.py
+++ script/coordinate.py
@@ -8,15 +8,16 @@ def translate(data,trans_x,trans_y):
     return ret
 
 def rotate(data,rad):
-    X = data[:,1]*settings.xp.cos(rad) - data[:,0]*settings.xp.sin(rad)
-    Y = data[:,1]*settings.xp.sin(rad) + data[:,0]*settings.xp.cos(rad)
+    X = data[:,0]*settings.xp.cos(rad) - data[:,1]*settings.xp.sin(rad)
+    Y = data[:,0]*settings.xp.sin(rad) + data[:,1]*settings.xp.cos(rad)
     TH = data[:,2] + rad
     ret = settings.xp.stack((X,Y,TH),axis=1)
     return ret
 
 def globalpos_to_localpos(data,selfpos):
     x, y, th = selfpos
-    ret = rotate(data,-th)
+    ret = data
     ret = translate(ret,x,y)
+    ret = rotate(ret,-th)
     return ret
 
